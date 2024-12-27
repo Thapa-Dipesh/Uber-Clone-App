d9401ece2f066799bdff5c60d8703c1a01b7bac0
Thapa-Dipesh <dthapa@4682>
2024-12-27 11:42:33 +0545

    login, signup page for user and captain

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<UserLogin />}/>
+        <Route path='/signup' element={<UserSignup />}/>
+        <Route path='/captain-login' element={<CaptainLogin />}/>
+        <Route path='/captain-signup' element={<CaptainSignup />}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ Frontend/src/pages/CaptainSignup.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const CaptainSignup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userData, setUserData] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({
+      FullName: {
+        firstName: firstName,
+        lastName: lastName,
+      },
+      email: email,
+      password: password,
+    });
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className=" h-screen p-7 flex flex-col justify-between">
+      <div>
+        <img
+          className="w-24"
+          src="https://www.svgrepo.com/show/505031/uber-driver.svg"
+          alt=""
+        />
+
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}>
+          <h3 className="text-lg font-medium mb-2">What's our Captain's name?</h3>
+          <div className="flex gap-3 mb-5">
+            <input
+              className="bg-[#eeeeee] rounded px-4 py-2 border w-1/2 text-lg placeholder:text-base"
+              type="text"
+              required
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+              placeholder="Enter First Name"
+              gap-4
+            />
+            <input
+              className="bg-[#eeeeee] rounded px-4 py-2 border w-1/2 text-lg placeholder:text-base"
+              type="text"
+              required
+              value={lastName}
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+              placeholder="Enter Last Name"
+            />
+          </div>
+
+          <h3 className="text-lg font-medium mb-2">What's our Captain's email?</h3>
+          <input
+            className="bg-[#eeeeee] rounded mb-5 px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="email"
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="email@example.com"
+          />
+
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            className="bg-[#eeeeee] rounded mb-5 px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="password"
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="password"
+          />
+
+          <button className="bg-[#111111] text-white font-semibold rounded mb-4 px-4 py-2 w-full text-lg placeholder:text-base">
+            Signup
+          </button>
+        </form>
+
+        <p className="text-center">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-600">
+            Login Here
+          </Link>
+        </p>
+      </div>
+
+      <div>
+        <Link
+          to="/signup"
+          className="bg-green-600 text-white flex items-center justify-center font-semibold rounded mb-5 px-4 py-2 w-full text-lg placeholder:text-base">
+          Sign up as User
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainSignup;
