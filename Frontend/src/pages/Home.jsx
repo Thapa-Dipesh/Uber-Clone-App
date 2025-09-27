21280bc9f85f7205e2e15d512f89e6c61a5f605d
Thapa-Dipesh <dthapa4682@gmail.com>
2025-09-27 20:50:12 +0545

    home page design

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { useState } from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [panelOpen, setPanelOpen] = useState(false);
 
-export default Home
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="h-screen relative">
+      <img
+        className="w-16 absolute left-5 top-5"
+        src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+        alt="uber img"
+      />
+
+      <div className="h-screen w-screen">
+        {/* image for temporary use */}
+        <img
+          className="h-full w-full object-cover"
+          src="https://www.medianama.com/wp-content/uploads/2018/06/Screenshot_20180619-112715.png.png"
+          alt=""
+        />
+      </div>
+
+      <div className="h-screen flex flex-col justify-end absolute top-0 w-full">
+        <div className="h-[30%] bg-white p-5 relative">
+          <h4 className="text-2xl font-semibold">Find a trip</h4>
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+          >
+            <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-900 rounded-full"></div>
+            <input
+              onClick={() => {
+                setPanelOpen(true);
+              }}
+              className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5"
+              type="text"
+              placeholder="Add a pick-up location"
+              value={pickup}
+              onChange={(e) => {
+                setPickup(e.target.value);
+              }}
+            />
+            <input
+              onClick={() => {
+                setPanelOpen(true);
+              }}
+              className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3"
+              type="text"
+              placeholder="Enter your destination"
+              value={destination}
+              onChange={(e) => {
+                setDestination(e.target.value);
+              }}
+            />
+          </form>
+        </div>
+
+        <div className="h-[0%] bg-red-500"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
