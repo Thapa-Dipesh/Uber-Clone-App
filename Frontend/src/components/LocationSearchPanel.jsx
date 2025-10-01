5589e6d8508262c2e91eeb4a2e46a611c85cec94
Thapa-Dipesh <dthapa4682@gmail.com>
2025-10-01 12:55:11 +0545

    creating search panel ui

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+      <div className='flex items-center justify-start my-3 gap-4'>
+        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>Los Angeles, CA 90001</h4>
+      </div>
+
+      <div className='flex items-center justify-start my-3 gap-4'>
+        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>Los Angeles, CA 90001</h4>
+      </div>
+
+      <div className='flex items-center justify-start my-3 gap-4'>
+        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>Los Angeles, CA 90001</h4>
+      </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
