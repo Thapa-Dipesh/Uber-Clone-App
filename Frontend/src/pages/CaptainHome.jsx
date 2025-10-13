d38a0fa54fd6ee64cdf932b921691746ad885ef1
Thapa-Dipesh <dthapa4682@gmail.com>
2025-10-13 14:55:45 +0545

    optimized captain home page with different component

--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import CaptainsDetails from "../components/CaptainsDetails";
 
 const CaptainHome = () => {
   return (
@@ -26,40 +27,7 @@ const CaptainHome = () => {
       </div>
 
       <div className="h-2/5 p-6">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center justify-start gap-3">
-            <img
-              className="h-10 w-10 rounded-full object-cover"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0xTwVADgFe4Vz1XzuMukOEKLXGcaanosc8Q&s"
-              alt="driver-image"
-            />
-            <h4 className="font-medium text-lg">Captain</h4>
-          </div>
-          <div>
-            <h4 className="text-xl font-semibold">&#8377;191.4</h4>
-            <p className="text-sm text-gray-600">Earned</p>
-          </div>
-        </div>
-
-        <div className="flex justify-center items-start gap-4 p-3 mt-6 bg-gray-100 rounded-xl">
-          <div className="text-center ">
-            <i className="ri-timer-2-line mb-2 text-3xl font-thin"></i>
-            <h5 className="text-lg font-medium">10.5</h5>
-            <p className="text-sm text-gray-600">Hours Online</p>
-          </div>
-
-          <div className="text-center ">
-            <i className="ri-speed-up-lin mb-2e text32xl font-thin"></i>
-            <h5 className="text-lg font-medium">10.5</h5>
-            <p className="text-sm text-gray-600">Hours Online</p>
-          </div>
-
-          <div className="text-center ">
-            <i className="ri-booklet-line mb-2 text-3xl font-thin"></i>
-            <h5 className="text-lg font-medium">10.5</h5>
-            <p className="text-sm text-gray-600">Hours Online</p>
-          </div>
-        </div>
+        <CaptainsDetails />
       </div>
     </div>
   );
