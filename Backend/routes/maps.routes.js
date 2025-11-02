395d728510f3aec7130a8ac389e2f4f6237b8c94
Thapa-Dipesh <dthapa4682@gmail.com>
2025-11-02 20:49:14 +0545

    map route

---
+++ Backend/routes/maps.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const { query } = require("express-validator");
+
+const authMiddleware = require("../middlewares/auth.middleware");
+const mapController = require("../controllers/map.controller");
+
+router.get(
+  "/get-coordinates",
+  query("address").isString().isLength({ min: 3 }),
+  authMiddleware.authUser,
+  mapController.getCoordinates
+);
+
+module.exports = router;
